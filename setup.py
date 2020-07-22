d69c48e01d519e35ac6096cd7b11454d6ea4acb9
Carlosgg <carlosgilglez@gmail.com>
2020-07-22 18:41:39 +0100

    updated setup.py, LICENSE

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='bleak_sigspec',
       version='0.0.1',
       description='SIG Bluetooth Low Energy Characteristic Metadata',
-      url='http://github.com/cgglz/test_bleak_sigspec',
+      url='http://github.com/cgglz/bleak_sigspec',
       author='Carlos Gil Gonzalez',
       author_email='carlosgilglez@gmail.com',
       license='MIT',
